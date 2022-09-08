bf895563df79557d02c0a82c2ce55d7974d3c254
Gyula <bogar.gyula22@gmail.com>
2022-09-08 18:08:31 +0200

    masodik

--- src/main/java/com/example/salescheckerspring/models/Admin.java
+++ src/main/java/com/example/salescheckerspring/models/Admin.java
@@ -1,4 +1,64 @@
 package com.example.salescheckerspring.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Admin {
+    @Id
+    @GeneratedValue
+    private Long Id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    public Admin(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Admin(Long id) {
+        Id = id;
+    }
+
+    public Admin() {
+
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
