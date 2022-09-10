870578bf291941a12909428ad03baec351a386cf
Zsombor Leber <leberzsombor96@gmail.com>
2022-09-10 10:41:07 +0200

    register,

--- src/main/java/com/example/salescheckerspring/Configs/WebSecurityConfig.java
+++ src/main/java/com/example/salescheckerspring/Configs/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.salescheckerspring.Configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .usernameParameter("email")
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .permitAll();
+    }
 
 }
--- src/main/java/com/example/salescheckerspring/services/UserService.java
+++ src/main/java/com/example/salescheckerspring/services/UserService.java
@@ -2,11 +2,14 @@ package com.example.salescheckerspring.services;
 
 import com.example.salescheckerspring.models.User;
 import com.example.salescheckerspring.repos.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
-    private UserRepository userRepository;
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -16,4 +19,12 @@ public class UserService {
       userRepository.save(user);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findAllByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return userRepository.findAllByEmail(email);
+    }
 }
