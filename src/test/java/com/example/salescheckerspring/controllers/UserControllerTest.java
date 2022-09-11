ee965c62e5a00e790f14fa1465d2a75841223566
Gyula <bogar.gyula22@gmail.com>
2022-09-11 17:34:54 +0200

    Uj tesztek.

--- src/test/java/com/example/salescheckerspring/controllers/UserControllerTest.java
+++ src/test/java/com/example/salescheckerspring/controllers/UserControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,4 +34,22 @@ public class UserControllerTest {
                 .andExpect(status().is(403));
     }
 
-}
+    /// Nem futnak le. Bad credentials issue.
+
+  /*  @Test
+    void login_as_user() throws Exception {
+        mockMvc.perform(
+                        SecurityMockMvcRequestBuilders.formLogin().user("Gyula@sales.com").password("kiscica"))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+    } */
+   @Test
+    void login_with_invalid_credentials() throws Exception {
+        mockMvc.perform(
+                        SecurityMockMvcRequestBuilders.formLogin().user("Gyula@sales.com").password("kiscicaa"))
+                .andExpect(status().is(302));
+
+        mockMvc.perform(
+                        SecurityMockMvcRequestBuilders.formLogin().user("aaa").password("aaa"))
+                .andExpect(status().is(302));
+    }
+ }
