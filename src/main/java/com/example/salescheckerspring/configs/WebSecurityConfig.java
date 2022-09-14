c4954cf89f8b94fc7d15ada57d7950372f3e0fd9
ButcherBence <bence.butcher@gmail.com>
2022-09-14 18:35:11 +0200

    Verify design

--- src/main/java/com/example/salescheckerspring/configs/WebSecurityConfig.java
+++ src/main/java/com/example/salescheckerspring/configs/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/").authenticated()
+                .antMatchers("/home").authenticated()
                 .antMatchers("/admin","/admin/").hasAuthority("ADMIN")
                 .anyRequest().permitAll()
                 .and()
