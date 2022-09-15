2d18d4121f0cd979e6a928352a71876b8be82c9a
ButcherBence <bence.butcher@gmail.com>
2022-09-15 17:47:56 +0200

    users

--- src/main/java/com/example/salescheckerspring/controllers/AppController.java
+++ src/main/java/com/example/salescheckerspring/controllers/AppController.java
@@ -1,10 +1,14 @@
 package com.example.salescheckerspring.controllers;
 
+import com.example.salescheckerspring.models.User;
 import com.example.salescheckerspring.services.ProductPastService;
 import com.example.salescheckerspring.services.ProductService;
+import com.example.salescheckerspring.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -12,9 +16,12 @@ public class AppController {
     private ProductPastService productPastService;
     private ProductService productService;
 
-    public AppController(ProductPastService productPastService, ProductService productService) {
+    private UserService userService;
+
+    public AppController(ProductPastService productPastService, ProductService productService, UserService userService) {
         this.productPastService = productPastService;
         this.productService = productService;
+        this.userService = userService;
     }
 
     @GetMapping(value={"/","/index"})
@@ -27,5 +34,13 @@ public class AppController {
     @GetMapping(value={"/admin"})
     private String adminPage(Model model){
         return "admin";
+    }
+
+    @GetMapping("/admin/users")
+    private String allUser(Model model){
+        List<User> users = userService.findAllUser();
+        model.addAttribute("users",users);
+
+        return "admin_users";
     }
 }
